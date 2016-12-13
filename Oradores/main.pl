2cac0cf92abed52da9347672804717e8d641c3c5
F0lha <pedrovieiracastro@hotmail.com>
2016-12-13 23:38:19 +0000

    list created

--- Oradores/main.pl
+++ Oradores/main.pl
@@ -1,22 +1,35 @@
 :- use_module(library(lists)).
 :- use_module(library(random)).
-:- use_module(library(between)).
 :- use_module(library(clpfd)).
 
-:initialization(menu).
 
-menu:- nl, write('Welcome To Speakrs'),nl
+
+menu:- nl, write('Welcome To Speakers'),nl,
 nl,
-write('1 - Original Puzzle '),nl,
-write('2 - Random Graph '),nl,
-write('3 - Credits '),nl,
-write('4 - Exit '),nl,
+write('1 - Organize Speakers '),nl,
+write('2 - Credits '),nl,
+write('3 - Exit '),nl,
 read(Answer), menuAnswer(Answer).
 
-menuAnswer(1):-chooseTypeOfLabeling(Type),chooseTimeOut(TimeOut),originalGame(Type,TimeOut),!,menu.
-menuAnswer(2):-randomGraphingMenu,!,menu.
-menuAnswer(3):-credits,!,menu.
-menuAnswer(4):-!.
+menuAnswer(1):-mainPred,nl,nl,!,menu.
+menuAnswer(2):-credits,!,menu.
+menuAnswer(1):-!.
 menuAnswer(_):-write('Wrong Input'),nl,nl,!,menu.
 
 credits:-nl, write('Puzzle implementation made by Carolina Moreira'),nl, nl.
+
+
+mainPred:-daysOfConference(Days),lecturePerDay(Lectures), SizeOfList is Days*Lectures, write(SizeOfList), length(ListOfLectures,SizeOfList).
+
+daysOfConference(Days):-	nl,write('How many days of Conference?'),nl,
+						read(Answer), daysOfConferenceAnswer(Answer,Days).
+						
+daysOfConferenceAnswer(Answer,Answer):-number(Answer), Answer > 0, Answer <7.
+daysOfConferenceAnswer(_,Days):-write('Wrong Input'),nl,nl,!,daysOfConference(Days).
+
+
+lecturePerDay(Lectures):-	nl,write('How many lectures of Conference?'),nl,
+						read(Answer), daysOfConferenceAnswer(Answer,Lectures).
+						
+lecturePerDay(Answer,Answer):-number(Answer), Answer > 0, Answer <7.
+lecturePerDay(_,Lectures):-write('Wrong Input'),nl,nl,!,lecturePerDay(Lectures).
---
+++ Oradores/oradores.pl
@@ -0,0 +1,16 @@
+%oradores
+
+
+orador("Pedro",portugal,50,masculino).
+
+orador("Andre",brazil,200,masculino).
+
+orador("Mafalda",portugal,80,feminino).
+
+orador("Mariana",brazil,250,feminino).
+
+orador("Sergio",espanha,150,masculino).
+
+orador("Sara",espanha,150,feminino).
+
+orador("Pedro",portugal,50).
